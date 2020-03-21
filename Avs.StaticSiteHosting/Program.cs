70e177a718cc230f28f6d2b28280e16b4be55fc4
Alexander Flanchik <alex.svs.fl@gmail.com>
2020-03-21 21:24:23 +0200

    Rename

--- Avs.StaticSiteHosting/Program.cs
+++ Avs.StaticSiteHosting/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Avs.SiteHoster.Web
+namespace Avs.StaticSiteHosting
 {
     public class Program
     {
--- Avs.StaticSiteHosting/Startup.cs
+++ Avs.StaticSiteHosting/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Avs.SiteHoster.Web
+namespace Avs.StaticSiteHosting
 {
     public class Startup
     {
@@ -32,7 +32,7 @@ namespace Avs.SiteHoster.Web
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("AVS Hosting service is running!");
+                    await context.Response.WriteAsync("AVS static site hosting service is running!!");
                 });
             });
         }
