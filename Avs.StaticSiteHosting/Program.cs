7f4282c3364508f061e1f8ed012b143cb151e1bc
AlexanderFlanchik <alex.svs.fl@gmail.com>
2020-04-02 14:27:22 +0300

    Content storage initialization

--- Avs.StaticSiteHosting/Program.cs
+++ Avs.StaticSiteHosting/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -11,9 +12,24 @@ namespace Avs.StaticSiteHosting
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public async static Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            InitStorage();
+
+            await host.RunAsync();
+        }
+
+        private static void InitStorage()
+        {
+            var contentPath = Path.Combine("Content");
+            if (!Directory.Exists(contentPath))
+            {
+                Directory.CreateDirectory(contentPath);
+                Console.WriteLine("Content folder created!");
+            } else {
+                Console.WriteLine($"Found static content storage: {contentPath}");
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
