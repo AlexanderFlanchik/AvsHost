980707c74a8fbe5f53a2e6767ae7c3137d44cc2d
Alexander Flanchik <alex.svs.fl@gmail.com>
2022-02-14 11:54:58 +0200

    API exception handling

--- Avs.StaticSiteHosting/Controllers/BaseUserIdController.cs
+++ Avs.StaticSiteHosting/Controllers/BaseUserIdController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Threading.Tasks;
 
 namespace Avs.StaticSiteHosting.Web.Controllers
 {
+    [ApiExceptionHandleFilter]
     public abstract class BaseController : Controller
     {
         private string _currentUserId;
@@ -19,6 +22,25 @@ namespace Avs.StaticSiteHosting.Web.Controllers
             }
 
             _currentUserId = userId;
+        }
+    }
+
+    public class ApiExceptionHandleFilter : Attribute, IAsyncExceptionFilter
+    {
+        public Task OnExceptionAsync(ExceptionContext context)
+        {
+            context.ExceptionHandled = true;
+            var errMessage = context.Exception.Message;
+            var problemDetails = new ProblemDetails()
+            {
+                Title = "Server Error",
+                Detail = errMessage,
+                Status = 500
+            };
+
+            context.Result = new ObjectResult(problemDetails);
+
+            return Task.CompletedTask;
         }
     }
 }
