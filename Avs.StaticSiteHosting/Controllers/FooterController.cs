46729a2ec8fe31fb5da222f7d31450f1f9cee02a
Oleksandr Flanchyk <alex.svs.fl@gmail.com>
2022-12-30 20:55:25 +0200

    Footer changes

---
+++ Avs.StaticSiteHosting/Controllers/FooterController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Avs.StaticSiteHosting.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class FooterController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index() => PartialView("__FooterPartial");
+    }
+}
