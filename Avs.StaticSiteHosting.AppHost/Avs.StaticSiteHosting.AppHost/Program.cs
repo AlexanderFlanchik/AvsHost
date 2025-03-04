851b08bfb3fc5fdc170c32c5468438536e5bdcc5
Alexander Flanchik <alex.svs.fl@gmail.com>
2025-03-04 10:15:10 +0200

    Orchestrator added RabbitMq

--- Avs.StaticSiteHosting.AppHost/Avs.StaticSiteHosting.AppHost/Program.cs
+++ Avs.StaticSiteHosting.AppHost/Avs.StaticSiteHosting.AppHost/Program.cs
@@ -8,9 +8,20 @@ if (string.IsNullOrEmpty(databaseName))
 
 var mongo = builder.AddMongoDB("mongo");
 var mongoDb = mongo.AddDatabase(databaseName);
+var rabbitMq = builder.AddRabbitMQ("avs_broker");
 
-builder.AddProject<Projects.Avs_StaticSiteHosting_Web>("avs-staticsitehosting-web")
+builder.AddProject<Projects.Avs_StaticSiteHosting_DataMigrator>("avs-staticsitehosting-datamigrator")
     .WithReference(mongoDb)
     .WaitFor(mongoDb);
+
+builder.AddProject<Projects.Avs_StaticSiteHosting_Web>("avs-staticsitehosting-web")
+     .WithReference(mongoDb)
+     .WaitFor(mongoDb)
+     .WithReference(rabbitMq)
+     .WaitFor(rabbitMq);
+
+builder.AddProject<Projects.Avs_StaticSiteHosting_ContentHost>("avs-staticsitehosting-contenthost")
+     .WithReference(rabbitMq)
+     .WaitFor(rabbitMq);
 
 builder.Build().Run();
