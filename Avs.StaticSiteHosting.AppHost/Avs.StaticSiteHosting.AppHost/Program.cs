ce1ed9283cf8a27b9912271824fd30d3dbefd414
Oleksandr Flanchyk <alex.svs.fl@gmail.com>
2025-03-04 09:33:10 +0200

    Orchestration started

--- Avs.StaticSiteHosting.AppHost/Avs.StaticSiteHosting.AppHost/Program.cs
+++ Avs.StaticSiteHosting.AppHost/Avs.StaticSiteHosting.AppHost/Program.cs
@@ -1,5 +1,16 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Avs_StaticSiteHosting_Web>("avs-staticsitehosting-web");
+var databaseName = builder.Configuration["Parameters:database_name"];
+if (string.IsNullOrEmpty(databaseName))
+{
+    throw new InvalidOperationException("Database name is required.");
+}
+
+var mongo = builder.AddMongoDB("mongo");
+var mongoDb = mongo.AddDatabase(databaseName);
+
+builder.AddProject<Projects.Avs_StaticSiteHosting_Web>("avs-staticsitehosting-web")
+    .WithReference(mongoDb)
+    .WaitFor(mongoDb);
 
 builder.Build().Run();
