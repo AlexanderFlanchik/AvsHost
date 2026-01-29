8f17db4af1bd8defc2b588f074911b242f2c1bbe
Oleksandr Flanchyk <alex.svs.fl@gmail.com>
2026-01-29 10:15:38 +0200

    Default host

--- Avs.StaticSiteHosting.CustomRouteApi/index.ts
+++ Avs.StaticSiteHosting.CustomRouteApi/index.ts
@@ -17,6 +17,7 @@ await handlerProvider.loadHandlers();
 
 const server = Bun.serve({
     port: servicePort,
+    hostname: "0.0.0.0",
     routes: {
         "/health": {
             GET: async (_) => Response.json({ status: "healthy" })
